12d733501290f5f2d7af269aefad718be2c768a1
ext.liuhaibo <ext.liuhaibo@jd.com>
2021-12-02 19:02:50 +0800

    初始

---
+++ user/src/main/java/com/microservices/user/dao/UserDao.java
@@ -0,0 +1,4 @@
+package com.microservices.user.dao;
+
+public interface UserDao {
+}
---
+++ web/src/main/java/com/microservice/web/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.microservice.web.controller;
+
+import com.microservice.common.userApi.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class UserController {
+
+//    @Autowired
+//    private UserService userService;
+
+
+    @GetMapping("/userTestMethod")
+    public  String  testMethod(){
+        return "";
+    }
+}
